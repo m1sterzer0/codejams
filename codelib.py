55276629fb1cf2022809d5c35dfc4b4dd94511ed
David Bradley <debr5836@gmail.com>
2018-04-13 15:37:18 -0600

    graph algos

--- codelib.py
+++ codelib.py
@@ -6,7 +6,7 @@ class minheap(object) :
     def push(self,a)   : heapq.heappush(self.h,a)
     def pop(self)      : return heapq.heappop(self.h)
     def top(self)      : return self.h[0]
-    def empty(self)    : return True if self.h else False
+    def empty(self)    : return False if self.h else True
 
 ## This assumes you use numbers.  Wrap a class and overload comparison operations for maxheap with nonstandard stuff
 class maxheap(object) :
@@ -14,7 +14,7 @@ class maxheap(object) :
     def push(self,a)   : heapq.heappush(self.h,-a)
     def pop(self,a)    : return -heapq.heappop(self.h)
     def top(self,a)    : return -self.h[0]
-    def empty(self)    : return True if self.h else False
+    def empty(self)    : return False if self.h else True
 
 class unionFind(object) :
     def __init__(self) :
@@ -72,7 +72,7 @@ class graph1(object) :
         for n in self.nodes : d[n] = 1e99
         d[src] = 0
         numnodes = len(self.nodes)
-        edgelist = [(x,y,self.g[x][y]) for y in self.g[x] for x in self.nodes]
+        edgelist = [(x,y,self.g[x][y]) for x in self.nodes for y in self.g[x]]
         for _ in range(numnodes-1) :
             for (x,y,dist) in edgelist :
                 if d[x] < 1e99 and d[x] + dist < d[y] : d[y] = d[x] + dist
@@ -102,10 +102,11 @@ class graph1(object) :
         parent = {}
         for n in self.g : parent[n] = n
         s = set()
-        h = minheap(); h.push((0,src,-1))
+        h = minheap(); h.push((0,src,src))
         while not h.empty() :
             (_,n,p) = h.pop()
             if n in s : continue
+            s.add(n)
             parent[n] = p
             for nn in self.g[n] :
                 if nn in s : continue
@@ -117,7 +118,7 @@ class graph1(object) :
         return mst
 
     def kruskal(self) :
-        edgelist = [(x,y,self.g[x][y]) for y in self.g[x] for x in self.nodes]
+        edgelist = [(x,y,self.g[x][y]) for x in self.nodes for y in self.g[x]]
         sortedEdgeList = sorted(edgelist,key=lambda x: x[2])
         mst = []; targetEdges = len(self.nodes)-1
         uf = unionFind()
@@ -166,3 +167,79 @@ class graph1(object) :
                 else              : self.g[s][u] =  pathFlow
                 s = parent[s]
         return maxFlow
+
+def _tc1() :
+    ## From https://www.cs.princeton.edu/~rs/AlgsDS07/15ShortestPaths.pdf
+    n1 = ['s', 2, 3, 4, 5, 6, 7, 't']
+    e1 = [ ('s', 2, 9),
+           ('s', 6, 14),
+           ('s', 7, 15),
+           (6, 3, 18),
+           (3, 5, 2),
+           (5,4,11),
+           (5,'t',16),
+           (6,7,5),
+           (7,5,20),
+           (6,5,30),
+           (4,3,6),
+           (4,'t',6),
+           (3,'t',19),
+           (7,'t',44),
+           (2,3,24)
+        ]
+    g1 = graph1(n1,e1)
+    d = g1.dijkstra('s')
+    d2,_ = g1.bellmanFord('s')
+    d3 = g1.floydWarshall()
+    print("_tc1:")
+    for n in n1 : print("%s --> %s: %d %d %d" % ('s', str(n), d[n], d2[n], d3['s'][n]))
+
+def _tc2() :
+    n1 = [1, 2, 3, 4, 5, 6, 7, 8]
+    e1 = [ (1, 2, 9),
+           (1, 6, 14),
+           (1, 7, 15),
+           (6, 3, 18),
+           (3, 5, 2),
+           (5,4,11),
+           (5,8,16),
+           (6,7,5),
+           (7,5,20),
+           (6,5,30),
+           (4,3,6),
+           (4,8,6),
+           (3,8,19),
+           (7,8,44),
+           (2,3,24)
+        ]
+    g1 = graph1(n1,e1,bidir=True)
+    t1 = g1.prim()
+    print(sorted(t1))
+    t2 = g1.kruskal()
+    print(sorted(t2))
+
+def _tc3() :
+    n1 = [1, 2, 3, 4, 5, 6, 7, 8]
+    e1 = [ (1, 2, 9),
+           (1, 6, 14),
+           (1, 7, 15),
+           (6, 3, 18),
+           (3, 5, 2),
+           (5,4,11),
+           (5,8,16),
+           (6,7,5),
+           (7,5,20),
+           (6,5,30),
+           (4,3,6),
+           (4,8,6),
+           (3,8,19),
+           (7,8,44),
+           (2,3,24) ]
+    g1 = graph1(n1,e1)
+    f = g1.fordFulkerson(1,8)
+    print(f)
+
+if __name__ == "__main__" :
+    _tc1()
+    _tc2()
+    _tc3()
