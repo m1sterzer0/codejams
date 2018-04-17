997a4b56fe4bd7a492d3317b775af1e6dfbafe34
David Bradley <debr5836@gmail.com>
2018-04-17 17:47:19 -0600

    finally finished 2009-1B

--- 2009/1B/C.py
+++ 2009/1B/C.py
@@ -1,6 +1,6 @@
 import sys
 import math
-import operator
+from operator import itemgetter
 class myin(object) :
     def __init__(self,default_file=None,buffered=False) :
         self.fh = sys.stdin
@@ -41,7 +41,7 @@ def analyzeSquare(w,sq) :
                 if '+' in signs :
                     posarr[i][j] = True
                     maxpos = max(maxpos,sq[i][j])
-                elif '+' in signs :
+                if '-' in signs :
                     negarr[i][j] = True
                     maxneg = max(maxneg,sq[i][j])
     return posarr,negarr,maxpos,maxneg,neighbors
@@ -57,50 +57,61 @@ def evalStepsLeft(targ,maxpos,maxneg,stepsSoFar,sq,posFlag,negFlag) :
     if targ <  0 and sq > 0 and negFlag and -targ % sq == 0 : locaMaxSteps = min(locMaxSteps,stepsSoFar+(-targ)//sq)
     return locMinSteps,locMaxSteps 
 
-def getAlternatives(i,j,val,level,resstr,sq,w) :
+def getSignAlternatives(i,j,resstr,sq,w) :
     res = []
     steps = [(-1,0),(1,0),(0,-1),(0,1)]
     for s1 in steps :
         signi = i + s1[0]; signj = j + s1[1]
         if signi < 0 or signi >= w or signj < 0 or signj >= w : continue
         sign = sq[signi][signj]
-        assert sign in ('+','-')
-        for s2 in steps :
-            numi = signi + s2[0], numj = j + signj
-            if numi < 0 or numi >= w or numj < 0 or numj >= w : continue
-            newSq = sq[numi][numj]
-            newVal = val + newSq if sign == '+' else val - newSq
-            res.append((numi,numj,newVal,level+1,resstr+sign+str(newSq)))
+        res.append((signi,signj,resstr+sign))
     return res
 
+def getNumAlternatives(i,j,val,level,resstr,sq,w) :
+    res = []
+    steps = [(-1,0),(1,0),(0,-1),(0,1)]
+    for s1 in steps :
+        numi = i + s1[0]; numj = j + s1[1]
+        if numi < 0 or numi >= w or numj < 0 or numj >= w : continue
+        sign = resstr[-1]
+        newSq = sq[numi][numj]
+        newVal = val + newSq if sign == '+' else val - newSq
+        res.append((numi,numj,newVal,level+1,resstr+str(newSq)))
+    return res
 
-def doQuery(sq,val,posarr,negarr,maxpos,maxneg,target) :
+def doQuery(sq,target,posarr,negarr,maxpos,maxneg) :
     w = len(sq) 
     maxSteps = 1e99
-    visited = {}
+    visited = set()
     queue = []
     for i in range(len(sq)) :
         for j in range(len(sq)) :
             if sq[i][j] in (0,1,2,3,4,5,6,7,8,9) :
-                queue.append((i,j,val,1,str(sq[i][j]))); visited.add((i,j,val))
+                queue.append((i,j,sq[i][j],1,str(sq[i][j]))); visited.add((i,j,sq[i][j]))
     
     ## Do the levels one by one and sort the queue after by string to keep this in the right order
+    ## -0 and +0 case is tricky , so need to do the signs, sort and then do the values to let -0 come before +0 cases to the same cell
     while True :
         oldqueue,queue = queue,[]
         oldqueue.sort(key=itemgetter(4))
-        for (i,j,val.level,resstr) in oldqueue :
-            (i,j,val,level,resstr) = queue.pop(0)
+        signqueue = []
+        for (i,j,val,level,resstr)  in oldqueue :
             if val == target : return resstr
-
             locMinSteps,locMaxSteps = evalStepsLeft(target-val,maxpos,maxneg,level,sq[i][j],posarr[i][j],negarr[i][j])            
             if locMinSteps > maxSteps : continue
             if locMaxSteps > 0 and locMaxSteps < maxSteps : maxSteps = locMaxSteps
-
-            alternatives = getAlternatives(i,j,val,level,resstr,sq,w)
-            for (ni,nj,nval,nlev,nresstr) in alternatives :
+            alternatives = getSignAlternatives(i,j,resstr,sq,w)
+            for (ni,nj,nresstr) in alternatives :
+                if (ni,nj,val) in visited : continue
+                visited.add((ni,nj,val))
+                signqueue.append((ni,nj,val,level,nresstr))
+        signqueue.sort(key=itemgetter(4))
+        for (i,j,val,level,resstr) in signqueue :
+            alternatives = getNumAlternatives(i,j,val,level,resstr,sq,w)
+            for (ni,nj,nval,nlevel,nresstr) in alternatives :
                 if (ni,nj,nval) in visited : continue
                 visited.add((ni,nj,nval))
-                queue.append((ni,nj,nval,nlev,nresstr))
+                queue.append((ni,nj,nval,nlevel,nresstr))
 
 if __name__ == "__main__" :
     IN = myin()
@@ -112,5 +123,5 @@ if __name__ == "__main__" :
         posarr,negarr,maxpos,maxneg,neighbors = analyzeSquare(w,sq)
         print("Case #%d:" % tt)
         for qq in qlist :
-            ans = doQuery(sq,qq,posarr,negarr,maxpos,maxneg,target)
+            ans = doQuery(sq,qq,posarr,negarr,maxpos,maxneg)
             print(ans)
