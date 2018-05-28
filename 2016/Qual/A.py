98dcc517fa1d23b5debccc8da8b76e79e583d3f0
David Bradley <debr5836@gmail.com>
2018-05-28 10:15:46 -0600

    2016/Qual done

---
+++ 2016/Qual/A.py
@@ -0,0 +1,65 @@
+import collections
+import functools
+import heapq
+import itertools
+import math
+import re
+import sys
+from fractions       import gcd
+from fractions       import Fraction
+from multiprocessing import Pool    
+from operator        import itemgetter
+
+class myin(object) :
+    def __init__(self,default_file=None,buffered=False) :
+        self.fh = sys.stdin
+        self.buffered = buffered
+        if(len(sys.argv) >= 2) : self.fh = open(sys.argv[1])
+        elif default_file is not None : self.fh = open(default_file)
+        if (buffered) : self.lines = self.fh.readlines()
+        self.lineno = 0
+    def input(self) : 
+        if (self.buffered) : ans = self.lines[self.lineno]; self.lineno += 1; return ans
+        return self.fh.readline()
+    def strs(self) :   return self.input().rstrip().split()
+    def ints(self) :   return (int(x) for x in self.input().rstrip().split())
+    def bins(self) :   return (int(x,2) for x in self.input().rstrip().split())
+    def floats(self) : return (float(x) for x in self.input().rstrip().split())
+
+def doit(fn=None,multi=False) :
+    IN = myin(fn)
+    t, = IN.ints()
+    inputs = [ getInputs(IN) for x in range(t) ]
+    if (not multi) : 
+        for tt,i in enumerate(inputs,1) :
+            ans = solve(i)
+            printOutput(tt,ans)
+    else :
+        with Pool(processes=32) as pool : outputs = pool.map(solve,inputs)
+        for tt,ans in enumerate(outputs,1) :
+            printOutput(tt,ans)
+
+#####################################################################################################
+
+def printOutput(tt,ans) :
+    print("Case #%d: %s" % (tt,ans))
+
+def getInputs(IN) :
+    n = int(IN.input())
+    return (n,)
+
+def solve(inp) :
+    (n,) = inp
+    if n == 0 : return "INSOMNIA"
+    s = set([str(x) for x in range(10)])
+    for k in range(1,101) : 
+        x = str(k * n)
+        for y in list(s) :
+            if y in x : s.remove(y)
+        if len(s) == 0 : return x
+    return "INSOMNIA"
+
+#####################################################################################################
+if __name__ == "__main__" :
+    doit()
+
