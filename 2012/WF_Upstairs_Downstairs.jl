c83c74009fb6ae7fe82f67e4c4069b9087025fa2
m1sterzer0 <debr5836@gmail.com>
2021-04-11 06:03:20 -0600

    updating common header

--- 2012/WF_Upstairs_Downstairs.jl
+++ 2012/WF_Upstairs_Downstairs.jl
@@ -4,6 +4,7 @@ infile = stdin
 ## Type Shortcuts (to save my wrists and fingers :))
 const I = Int64; const VI = Vector{I}; const SI = Set{I}; const PI = NTuple{2,I};
 const TI = NTuple{3,I}; const QI = NTuple{4,I}; const VPI = Vector{PI}; const SPI = Set{PI}
+const VC = Vector{Char}; const VS = Vector{String}; VB = Vector{Bool}; VVI = Vector{Vector{Int64}}
 const F = Float64; const VF = Vector{F}; const PF = NTuple{2,F}
 
 gs()::String = rstrip(readline(infile))
