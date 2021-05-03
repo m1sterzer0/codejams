87ebdcf1a8d3c54cab339a31374f8492429f34a6
m1sterzer0 <debr5836@gmail.com>
2021-05-02 18:40:45 -0600

    massive cleanup

--- old/2015/SelectedTestgen/1B_CounterCulture_tg.py
+++
@@ -1,5 +0,0 @@
-
-with open("Asmall.in","wt") as fp :
-    print(1000000,file=fp)
-    for i in range(1,1000001) :
-        print(i,file=fp)
--- work/2020/3/junk.jl
+++
@@ -1,3 +0,0 @@
-for i in 1:100
-    a::Array{Int32,4} = fill(Int32(0),2048*2048*4,4)
-end
